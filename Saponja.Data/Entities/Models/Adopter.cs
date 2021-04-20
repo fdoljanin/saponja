1f7263641d0533ad74c091675348254361408ea3
fdoljanin <doljaninfrane@gmail.com>
2021-04-20 23:29:20 +0200

    React axios configured

--- Saponja.Data/Entities/Models/Adopter.cs
+++ Saponja.Data/Entities/Models/Adopter.cs
@@ -16,6 +16,7 @@ namespace Saponja.Data.Entities.Models
         public string Motivation { get; set; }
         public string ConfirmationLink { get; set; }
         public bool HasConfirmedMail { get; set; }
+        public bool HasReceivedDocumentation { get; set; }
         public int AnimalId { get; set; }
         public Animal Animal { get; set; }
     }
