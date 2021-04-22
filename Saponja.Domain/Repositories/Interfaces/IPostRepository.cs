53a695730f88a499c77f6d71e6c52b3a88ea994d
„michellesaric“ <msaric03@fesb.hr>
2021-04-22 16:23:16 +0200

    Added actions for posts

--- Saponja.Domain/Repositories/Interfaces/IPostRepository.cs
+++ Saponja.Domain/Repositories/Interfaces/IPostRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Saponja.Domain.Abstractions;
+using Saponja.Domain.Models.ViewModels.Post;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,11 @@ namespace Saponja.Domain.Repositories.Interfaces
 {
     public interface IPostRepository
     {
+        ResponseResult CreatePost(PostModel model, int shelterId);
+        ResponseResult DeletePost(int shelterId);
+        ICollection<PostModel> GetPosts();
+        ResponseResult<PostModel> GetPost(int postId);
+        ICollection<PostModel> GetTopThreePosts();
     }
+}
 }
