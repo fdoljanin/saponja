1821faf23afb341c2ae041fc3e25341ab34cb0e3
„michellesaric“ <msaric03@fesb.hr>
2021-04-26 14:52:27 +0200

    added the photo gallery

--- Saponja.Web/ClientApp/src/components/AnimalDetails/index.js
+++ Saponja.Web/ClientApp/src/components/AnimalDetails/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Logo from "./Assets/logo.svg";
 import Background from './Assets/pozadina.svg';
+import FirstDog from './Assets/slike/pas 1.jpg';
+import SecondDog from './Assets/slike/pas 2.jpg';
+import ThirdDog from './Assets/slike/pas 3.jpg';
+import FourthDog from './Assets/slike/pas 4.jpg';
+import FifthDog from './Assets/slike/pas 5.jpg';
 import "./animalDetails.css";
 
 const AnimalDetails = () => {
@@ -22,7 +27,15 @@ const AnimalDetails = () => {
       <div className="animal__details">
         <img src={Background} alt="pozadina" className="animal__details-background"/>
         <div className="animal__details-container">
-          
+          <div className="animal__details-container-images">
+          <img src={FirstDog} alt="dog-image-one" className="first-dog"/>
+          <div className="animal__details-container-image-gallery">
+            <img src={SecondDog} alt="dog-image-two"/>
+            <img src={ThirdDog} alt="dog-image-three"/>
+            <img src={FourthDog} alt="dog-image-four"/>
+            <img src={FifthDog} alt="dog-image-five"/>
+          </div>
+          </div>
         </div>
       </div>
     </div>
